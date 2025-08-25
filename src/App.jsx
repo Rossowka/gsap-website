696c2479a46bb33e5a087288b54f3a49452a4c53
Sara Rossow <rossowka@icloud.com>
2025-08-25 14:10:23 +0200

    created first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import gsap from "gsap";
-import { ScrollTrigger, SplitText } from "gsap/all"
-import NavBar from "./components/NavBar";
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
-gsap.registerPlugin(ScrollTrigger, SplitText); // you only have to do it once to make sure the plugins are used globally - putting it in App file is the way to go
+gsap.registerPlugin(ScrollTrigger, SplitText) // you only have to do it once to make sure the plugins are used globally - putting it in App file is the way to go
 
 const App = () => {
-  return (
-    <main>
-      <NavBar />
-    </main>
-  )
+	return (
+		<main>
+			<NavBar />
+			<Hero />
+		</main>
+	)
 }
 
 export default App
