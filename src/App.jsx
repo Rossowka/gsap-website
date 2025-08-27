4c17378c8aad0332c6a6145ca97e878c2c5e18d6
Sara Rossow <rossowka@icloud.com>
2025-08-27 12:28:38 +0200

    implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
+import About from './components/About'
 
 gsap.registerPlugin(ScrollTrigger, SplitText) // you only have to do it once to make sure the plugins are used globally - putting it in App file is the way to go
 
@@ -10,6 +12,8 @@ const App = () => {
 		<main>
 			<NavBar />
 			<Hero />
+			<Cocktails />
+			<About />
 		</main>
 	)
 }
