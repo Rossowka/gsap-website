d2f756f334e8b7e7d1ef1490ab06f9cf13cd087b
Sara Rossow <rossowka@icloud.com>
2025-08-27 08:12:10 +0200

    Implement cocktails section

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const navLinks = [
+const navLinks = [
 	{
 		id: 'cocktails',
 		title: 'Cocktails',
@@ -14,5 +14,59 @@ export const navLinks = [
 	{
 		id: 'contact',
 		title: 'Contact',
+	},
+]
+
+const cocktailLists = [
+	{
+		name: 'Emerald Drop',
+		ingredients: 'Gin, Midori (melon liqueur), lime juice, elderflower syrup',
+		detail: 'Martini glass',
+		price: '€13',
+	},
+	{
+		name: 'Velvet Lagoon',
+		ingredients: 'White rum, blue curaçao, coconut cream, pineapple juice, lime',
+		detail: 'Hurricane glass',
+		price: '€11',
+	},
+	{
+		name: 'Midnight Mango Tango',
+		ingredients: 'Tequila, mango purée, triple sec, lime, agave syrup',
+		detail: 'Coupe glass',
+		price: '€12',
+	},
+	{
+		name: 'Crimson Drift',
+		ingredients: 'Vodka, hibiscus syrup, passion fruit, lemon, soda splash',
+		detail: 'Highball glass',
+		price: '€13',
+	},
+]
+
+const mockTailLists = [
+	{
+		name: 'Coconut Daydream',
+		ingredients: 'Coconut water, pineapple juice, vanilla syrup, lime',
+		detail: 'Highball glass',
+		price: '€7',
+	},
+	{
+		name: 'Sunset Serenade',
+		ingredients: 'Orange juice, cranberry, grenadine, tonic',
+		detail: 'Collins glass',
+		price: '€8',
+	},
+	{
+		name: 'Velvet Breeze',
+		ingredients: 'Passion fruit, ginger ale, mint, honey syrup',
+		detail: 'Rocks glass',
+		price: '€7',
+	},
+	{
+		name: 'Tropical Glow',
+		ingredients: 'Mango nectar, soda, basil leaves, lime',
+		detail: 'Stemless wine glass',
+		price: '€8',
 	},
 ]
